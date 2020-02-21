d705ace30c52eef912c15a8ed0ff9af573200b01
besi4lukas <besi4lukas@gmail.com>
2020-02-21 01:40:04 -0700

    added a main function

--- client.py
+++ client.py
@@ -1,2 +1,10 @@
 #This contains client source code
 #import socket
+#import sys
+
+
+
+
+
+if __name__ == '__main__' :
+    #main function for our code
--- server.py
+++ server.py
@@ -1,2 +1,13 @@
 #This file contains server source code
 #import socket
+#import sys
+
+
+
+
+
+
+if __name__ == '__main__' :
+    #main function for our code
+    
+    

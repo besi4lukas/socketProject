691cbeb0f10d69774a4f281a03601981d8b08cbe
besi4lukas <besi4lukas@gmail.com>
2020-02-21 16:03:09 -0700

    created initial empty functions

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #This contains client source code
-#import socket
-#import sys
+import socket
+import sys
 
 
 
@@ -10,4 +10,18 @@ if __name__ == '__main__' :
     #main function for our code
 
     #input from command line
+
+    #check if our input parameters are of correct length
+
+    #create socket for sending and recieving datagrams
+
+    #construct local address for server
+
+    #connect to local address and port
+
+    
+
+    
+
+    
     
--- server.py
+++ server.py
@@ -1,19 +1,40 @@
 #This file contains server source code
-#import socket
-#import sys
+import socket
+import sys
 
 
-#dictionary for storing user data
+#dictionary for storing registered nodes and their status
+
+#dictionary for storing nodes in dht
+
 
 #register function for registering users
+def register():
+    return "SUCCESS"
 
 #setup function for constructing the dht
+def setUp():
+    return "SUCCESS"
 
 #dht complete function to check if dht requirements are satisfied
+def dhtComplete():
+    return "SUCCESS"
 
-#query function for retriving information from dht 
+#query function for retriving information from dht
+def query():
+    return "SUCCESS"
 
 #Class nodes for client objects
+class node:
+    def __init__(self, username, ip_address, port):
+        self.username = username
+        self.ip_address = ip_address
+        self.port = port
+        #create local hash table for storing information
+
+    #create hash function
+    #define function for inserting into hash table
+    #define function for getting information
 
 
 if __name__ == '__main__' :
@@ -25,7 +46,7 @@ if __name__ == '__main__' :
 
     #create socket for sending and recieving datagrams
 
-    #Construct a local address
+    #Construct a local address for server
 
     #Bind the local address and port
 

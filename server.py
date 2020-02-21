e1a8305aefb83ffc33b7a810fa94cafe5d89d804
besi4lukas <besi4lukas@gmail.com>
2020-02-21 14:51:34 -0700

    adding pseudocode comments to server file

--- server.py
+++ server.py
@@ -10,4 +10,19 @@
 if __name__ == '__main__' :
     #main function for our code
     
+    #port integer input from command line
+    
+    #check if our input is at most one
+
+    #create socket for sending and recieving datagrams
+
+    #Construct a local address
+
+    #Bind the local address and port
+
+    #listen and accept 
+
+    
+    
+    
     
